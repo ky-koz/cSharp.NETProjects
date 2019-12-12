f49ecd66193afa7368accbcb2b34b2a50ae713a6
kylakozole <31643994+kylakozole@users.noreply.github.com>
2019-12-11 17:46:48 -0700

    drill 57

---
+++ drill57/drill57/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace drill57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string help = Console.ReadLine();
+            bool helpResponse = Convert.ToBoolean(help);
+            Console.WriteLine("Were there any experiences you would like to share? Please give specifics.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            int studyTime = Convert.ToInt32(hours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+
+
+        }
+    }
+}
