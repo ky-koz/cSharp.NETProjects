5d1e8bf67cff8d8e6c5c139a2b6b2592caa5093e
kylakozole <31643994+kylakozole@users.noreply.github.com>
2019-12-26 13:05:00 -0700

    loops and drill

---
+++ drill94/drill94/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill94
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You see $20 on the ground. You have five options:");
+            Console.WriteLine("Option 1: See if someone around you dropped it.");
+            Console.WriteLine("Option 2: Give it to a homeless person.");
+            Console.WriteLine("Option 3: Put it in your wallet, carry on with your day.");
+            Console.WriteLine("Option 4: Go and buy something with it.");
+            Console.WriteLine("Option 5: Do nothing, keep walking.");
+            Console.WriteLine("Please choose a response by typing the option number \"1-5\"");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool choice = number <= 5;
+
+            do
+            {
+                switch(number)
+                {
+                    case 1:
+                        Console.WriteLine("You chose Option 1. Way to be a good sumaritan.");
+                        choice = true;
+                        break;
+                    case 2:
+                        Console.WriteLine("You chose Option 2. You are a caring and generous person.");
+                        choice = true;
+                        break;
+                    case 3:
+                        Console.WriteLine("You chose Option 3. May as well.");
+                        choice = true;
+                        break;
+                    case 4:
+                        Console.WriteLine("You chose Option 4. Don't spend it all in one place!");
+                        choice = true;
+                        break;
+                    case 5:
+                        Console.WriteLine("You chose Option 5. Okay, maybe someone else will find it?");
+                        choice = true;
+                        break;
+                    default:
+                        Console.WriteLine("You did not choose a valid option.");
+                        Console.WriteLine("Please choose again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+            while (!choice);
+            Console.Read();
+        }
+    }
+}
---
+++ loops/loops/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62: // case is a defined word
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break; // loop will continue unless you break
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default: //if none of the cases are true than we have a default
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
+
+        }
+    }
+}
