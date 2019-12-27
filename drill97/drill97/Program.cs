a8cc3e5c0f16232582e293bb2db548ab8a44e862
kylakozole <31643994+kylakozole@users.noreply.github.com>
2019-12-26 18:01:01 -0700

    string functions and drill97

---
+++ drill97/drill97/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //concatenate three strings
+            string sOne = "This is ";
+            string sTwo = "a multiple ";
+            string sThree = "part sentence.";
+            string str;
+
+            //convert a string to uppercase
+            sTwo = sTwo.ToUpper();
+
+            str = String.Concat(sOne, sTwo, sThree);
+
+            //Create a Stringbuilder for a paragraph
+            StringBuilder sb = new StringBuilder("Hello, ", 100);
+
+            sb.Append("viewer. ");
+            sb.Append("I am creating a paragraph with the Stringbuilder.");
+            sb.Append("One line at a time. ");
+            sb.Append("Three sentences is the minimum for a paragraph. ");
+            sb.Append("This paragraph has exceeded the minimum sentence requirement.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ stringFunctions/stringFunctions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //    string name = "Jesse";
+            //    string quote = "The man said, \"Hello\", Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            //    string fileName = @"C:\Users\Jesse"; // @ means everything in quotations is string value, no escape sequences
+
+            //    bool trueOrFalse = name.Contains("s");
+            //    trueOrFalse = name.EndsWith("s");
+
+            //    int length = name.Length;
+
+            //    name = name.ToLower();
+
+            //    Console.WriteLine(name);
+            //    Console.ReadLine();
+
+            //string name = "Jesse";
+            //name = "Joe";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse"); // works similar to string
+
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+           
+        }
+    }
+}
