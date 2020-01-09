fe1ac6ece161c5c8a652464c301d9357bba2649d
kylakozole <31643994+kylakozole@users.noreply.github.com>
2020-01-09 12:54:39 -0700

    drill 147

--- drill147/drill147/Program.cs
+++ drill147/drill147/Program.cs
@@ -24,25 +24,48 @@ namespace drill147
                 Console.WriteLine(itemA);
             }
 
-            //asks user for int input to divide items in listA by
-            Console.WriteLine("\nPlease type an integer to divide each item of the list by: ");
-            int userInt = Convert.ToInt32(Console.ReadLine());
-            
-            //divide each item by user int
-            foreach (int itemA in listA)
+           
+
+
+            try
             {
-                int newItem = itemA / userInt;
-                Console.WriteLine("{0} divided by {1} equals {2}.", itemA, userInt, newItem);
+                //asks user for int input to divide items in listA by
+                Console.WriteLine("\nPlease type an integer to divide each item of the list by: ");
+                int userInt = Convert.ToInt32(Console.ReadLine());
+
+                //divide each item by user int
+                foreach (int itemA in listA)
+                {
+                    int newItem = itemA / userInt;
+                    Console.WriteLine("{0} divided by {1} equals {2}.", itemA, userInt, newItem);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); //will write out error message
             }
 
-
             Console.Read();
-                
+
             //2.Run that code, entering in non - zero numbers as the user.Look at the displayed results.
 
+            //Please type an integer to divide each item of the list by:
+            //    3
+            //    4 divided by 3 equals 1.
+            //    6 divided by 3 equals 2.
+            //    7 divided by 3 equals 2.
+            //    20 divided by 3 equals 6.
+            //    37 divided by 3 equals 12.
+            //    10 divided by 3 equals 3.
+            //    13 divided by 3 equals 4.
+            //    2 divided by 3 equals 0.
+            //the int does not use decimals, there are remainder values in the equations
 
             //3.Run that code, entering in zero as the number to divide by.Note any error messages you get.
 
+            //Unhandled Exception: System.DivideByZeroException: Attempted to divide by zero.
+            //at drill147.Program.Main(String[] args) in C: \Users\Kyla Kozole\Desktop\Repositories\c#.NET\
+            //drill147\drill147\Program.cs:line 34
 
             //4.Run that code, entering in a string as the number to divide by.Note any error messages you get.
 
@@ -52,6 +75,14 @@ namespace drill147
             //and continued on with program execution.In the catch block, display the error message to the screen.
             //Then try various combinations of user input: valid numbers, zero and a string.Ensure the proper 
             //error messages display on the screen, and that the code after the try/catch block gets executed.
+
+            //string
+            //g
+            //Input string was not in a correct format.
+
+            //zero
+            //0
+            //Attempted to divide by zero.
         }
     }
 }
