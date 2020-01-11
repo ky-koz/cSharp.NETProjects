77102edc7fe9ee301a5e580554bea467d3fa1047
kylakozole <31643994+kylakozole@users.noreply.github.com>
2020-01-11 13:04:02 -0700

    finished drill 156

--- drill156/drill156/Program.cs
+++ drill156/drill156/Program.cs
@@ -10,14 +10,25 @@ namespace drill156
     {
         static void Main(string[] args)
         {
-            //Do I need to create an object for the class methods to exist?
+            //Does this create an object for the class methods to exist?
+            Class1 operations = new Class1();
+
 
             Console.WriteLine("Choose a variable to perform math operations.");
             Console.WriteLine("Please enter a whole number: ");
-            int var1= Convert. ToInt32(Console.ReadLine());
-            
+            int var1 = Convert.ToInt32(Console.ReadLine());
+
+
             //I need an object to call, a method is not an object
-            Console.WriteLine(Class1.Multiply);
+            Console.WriteLine("{0} multiplied by itself :", var1);
+            Console.WriteLine(operations.Multiply(var1));
+
+            Console.WriteLine("{0} divided by itself :", var1);
+            Console.WriteLine(operations.Divide(var1));
+
+            Console.WriteLine("{0} added to itself :", var1);
+            Console.WriteLine(operations.Add(var1));
+            Console.ReadLine();
         }
         
     }
@@ -25,35 +36,3 @@ namespace drill156
 
 //2. In the Main() program, ask the user what number they want to do the math operations on.
 //3. Call each method in turn, passing the user input to the method.Display the returned integer to the screen.
-
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        Deck deck = new Deck();
-//        deck = Shuffle(deck); //call and return new deck value from method shuffle
-
-//        foreach (Card card in deck.Cards)
-//        {
-//            Console.WriteLine(card.Face + " of " + card.Suit);
-//        }
-//        Console.WriteLine(deck.Cards.Count);
-//        Console.ReadLine();
-//    }
-
-//    //randomly remove a cards from deck and put it into a temp deck, do that until deck count is at 0
-//    public static Deck Shuffle(Deck deck)
-//    {
-//        List<Card> TempList = new List<Card>();
-//        Random random = new Random();
-
-//        while (deck.Cards.Count > 0)
-//        {
-//            int randomIndex = random.Next(0, deck.Cards.Count);
-//            TempList.Add(deck.Cards[randomIndex]);
-//            deck.Cards.RemoveAt(randomIndex); //.removeAt is a list method
-//        }
-//        deck.Cards = TempList;
-//        return deck;
-//    }
-//}
--- twentyOne/twentyOne/Program.cs
+++ twentyOne/twentyOne/Program.cs
@@ -11,40 +11,50 @@ namespace twentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            deck = Shuffle(deck); //call and return new deck value from method shuffle
+            int timesShuffled = 0; //must create the object outside of the loop
+            //deck = Shuffle(deck, 3); //call and return new deck value from method shuffle, tells it to shuffle three times
+            //deck = Shuffle(deck: deck, times: 3);
+            deck = Shuffle(deck, out timesShuffled, 3);
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Time shuffled: {0}", timesShuffled);
             Console.ReadLine();
         }
 
         //randomly remove a cards from deck and put it into a temp deck, do that until deck count is at 0
-        public static Deck Shuffle(Deck deck)
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
         {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random();
-
-            while (deck.Cards.Count > 0)
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
             {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex); //.removeAt is a list method
+                timesShuffled++;
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex); //.removeAt is a list method
+                }
+                deck.Cards = TempList;
             }
-            deck.Cards = TempList;
+
             return deck;
         }
 
-        public static Deck Shuffle(Deck deck, int times)
-        {
-           for (int i = 0; i < times; i++)
-           {
-                deck = Shuffle(deck);
-           }
-           return deck;
-        }
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //   for (int i = 0; i < times; i++)
+        //   {
+        //        deck = Shuffle(deck);
+        //   }
+        //   return deck;
+        //}
     }
 }
 
