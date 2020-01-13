db9c1effda748b38715c6ed363791da9562ad202
kylakozole <31643994+kylakozole@users.noreply.github.com>
2020-01-13 14:54:45 -0700

    drill 161

--- drill160/drill160/Program.cs
+++ drill160/drill160/Program.cs
@@ -10,36 +10,27 @@ namespace drill160
     {
         static void Main(string[] args)
         {
-            //instantiate the class object
-            Class2 operations = new Class2();
+            Class1 combos = new Class1();
+
+            Console.WriteLine("Please enter a whole number for variable one... ");
+            int var1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Optional: Please enter a whole number for variable two or press enter to continue... ");
+            int var2 = Convert.ToInt32(Console.ReadLine());
 
 
-            Console.WriteLine("Choose a variable to perform math operations with.");
-            Console.WriteLine("Please enter a whole number... ");
-            int req = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Optional: Choose a second variable to perform math operations with.");
-            Console.WriteLine("Please enter a whole number or just press enter to continue... ");
-
-            //use tryparse instead of convert because an empty string is not a valid input for Convert.
-            //set data type
-            int opt;
-            int result;
-
-            //create if else for optional input
-            if (Int32.TryParse(Console.ReadLine(), out opt))
-            {
-                result = operations.Math(req, opt);
-                Console.WriteLine("{0} multiplied by {1} = {2}", req, opt, result);
-            }
-            else
-            {
-                result = operations.Math(req);
-                Console.WriteLine("{0} multiplied by the default second value 5 = {1}", req, result);
-            }
-            
-            Console.ReadLine();
-            
         }
     }
 }
+
+//1. Create a class. In that class, create a method that takes two integers as parameters.
+//Make one of them optional.Have the method do a math operation and return an integer result.
+
+//2. In the Main() method of the console app, instantiate the class.
+
+//3. Ask the user to input two numbers, one at a time.Let them know they need not enter anything 
+//for the second number.
+
+//4. Call the method in the class, passing in the one or two numbers entered.
+
+//5. Try various combinations of numbers on the code, including having no second number.
