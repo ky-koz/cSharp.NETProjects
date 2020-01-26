7ac51d37f91a73b4a7eea69ebee3da004c181887
kylakozole <31643994+kylakozole@users.noreply.github.com>
2020-01-26 15:06:47 -0700

    carInsurance progress

---
+++ CarInsuranceApp/CarInsuranceApp/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using CarInsuranceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsuranceApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CustomerInfo(string firstName, string lastName, string emailAddress, DateTime dateOfBirth,
+                                          int carYear, string carMake, string carModel, bool dui, int speedingTickets,
+                                          int insuranceType)
+        {
+            using (CarInsuranceEntities db = new CarInsuranceEntities())
+            {
+                var customerInfo = new CustomerInfo();
+                customerInfo.FirstName = firstName;
+                customerInfo.LastName = lastName;
+                customerInfo.EmailAddress = emailAddress;
+                customerInfo.DateOfBirth = dateOfBirth;
+                customerInfo.CarYear = carYear;
+                customerInfo.CarMake = carMake;
+                customerInfo.CarModel = carModel;
+                customerInfo.DUI = dui;
+                customerInfo.SpeedingTickets = speedingTickets;
+                customerInfo.InsuranceType = insuranceType;
+
+                db.CustomerInfoes.Add(customerInfo);
+                db.SaveChanges();
+            }
+
+            return View ("Success")
+        }
+        //{
+        //    if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) || DateTime.IsNullOrEmpty(dateOfBirth) ||
+        //        int (carYear) || string.IsNullOrEmpty(carMake) || string.IsNullOrEmpty(carModel) || bool.IsNullOrEmpty(dui) ||
+        //        int.IsNullOrEmpty(speedingTickets) || int.IsNullOrEmpty(insuranceType))
+        //    {
+        //        return ViewContext("~V/Views/Shared/Error.cshtml)
+        //    }
+        //}
+
+    }
+}
+
+//First Name
+
+//Last Name
+
+//Email Address
+
+//Date of Birth
+
+//Car Year
+
+//Car Make
+
+//Car Model
+
+//If they have ever had a DUI.
+
+//How many speeding tickets they have.
+
+//Would they like full coverage or liability?
---
+++ CarInsuranceApp/CarInsuranceApp/ViewModels/CustomerInfoVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsuranceApp.ViewModels
+{
+    public class CustomerInfoVm
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public int CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public bool DUI { get; set; }
+        public int SpeedingTickets { get; set; }
+        public int InsuranceType { get; set; }
+
+    }
+}
